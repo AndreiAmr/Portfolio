d7d6f036b9118150c6ff47f2dc2e2702692c09eb
Your Name <you@example.com>
2021-01-28 17:15:44 -0300

    Visual do Header feito para dispositivos mobile.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,36 @@
 import React from 'react';
-import { Container, GlobalStyle } from './appStyle';
+import { Container, GlobalStyle, Header } from './appStyle';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMoon } from "@fortawesome/free-solid-svg-icons";
+import { BrowserRouter } from 'react-router-dom';
+import Routes from './routes';
 
 function App() {
   return (
     <Container>
       <GlobalStyle />
-      <h1> Hello world</h1>
+      <Header>
+        <div className="header-area">
+          <h1> {'{'} Andrei  <b> Amaral {'}'} </b> </h1>
+
+          <FontAwesomeIcon icon={faMoon} />
+
+
+          <div className="menu-area">
+            <div className="menu-bar"></div>
+            <div className="menu-bar"></div>
+            <div className="menu-bar"></div>
+          </div>
+        </div>
+
+        <div className="page-title-container">
+          <h3> Home</h3>
+          <h1> Programador não por formação mas por vocação. </h1>
+        </div>
+      </Header>
+      <BrowserRouter>
+        <Routes />
+      </BrowserRouter>
     </Container>
   );
 }
--- src/appStyle.ts
+++ src/appStyle.ts
@@ -13,3 +13,83 @@ export const GlobalStyle = createGlobalStyle`
 export const Container = styled.div`
     width: 100%;
 `
+
+export const Header = styled.header`
+    display: flex;
+    flex-direction: column;
+
+    > .header-area { 
+        height: 55px;
+        width: 100%;
+        display: flex;
+        justify-content: space-around;
+        align-items: center;  
+        padding: 0;
+        > h1 {
+            font-family: 'Nunito';
+            font-size: 16px;
+            margin: 0;
+
+            > b {
+                color: #34F4F4;
+            }
+        }
+
+        > .menu-area {
+            height: 30px;
+            width: 40px;
+            border-radius: 10px;
+            background-color: #C4C4C429;
+
+            .menu-bar { 
+                height: 3px;
+                width: 27px;
+                margin: 5px auto;
+                background-color: #000;
+                border-radius: 15px;
+
+                &:nth-child(2){
+                    margin-left: 10px;
+                }
+            }
+            
+        }
+    }
+
+    > .page-title-container{
+        position: relative;
+        height: 130px;
+        width: 100%;
+        background-image: linear-gradient(to right, #22CDD8, #21F2FF);
+        overflow: hidden;
+
+        > h3 {
+            font-family: "Nunito";
+            font-size: 20px;
+            color: #ffffff79;
+            margin: 5px 10px;
+        }
+
+        > h1 {
+            font-family: "Nunito";
+            font-size: 23px;
+            color: #fff;
+            margin: 0 10px;
+            text-align: left;
+        }
+        
+        &:after {
+            position: absolute;
+            content: '';
+            width: 100%;
+            height: 50px;
+            background-color: #ffffff49;
+            bottom: -20px;
+            right: 0;
+            left: 0;
+            transform: rotate(-3deg);
+        }
+    }
+
+    
+`
