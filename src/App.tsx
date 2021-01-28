92ca02b7b7a1886a16ca6696742ad5db0586fac3
Your Name <you@example.com>
2021-01-28 14:12:44 -0300

    Primeiro commit. Preparando ambiente.

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Container, GlobalStyle } from './appStyle';
+
+function App() {
+  return (
+    <Container>
+      <GlobalStyle />
+      <h1> Hello world</h1>
+    </Container>
+  );
+}
+
+export default App;
